aa1ccff3a6c7b3b1bf466b4b2824ff426e8abdc8
yujiex <shenqing1225@gmail.com>
2018-06-29 17:36:30 -0400

    Messy edits to potential saving function
    - mutated Cat to character
    - format output and table
    - change to a top plot (orange "saving") and bottom plot (green "increase")
    - changed color to design people's color
    - unified font size and family

--- summarise.and.plot/R/plot.R
+++ summarise.and.plot/R/plot.R
@@ -579,26 +579,32 @@ dollar_saving <- function(category, type, year, region, method="own", topn=10, b
       readr::write_csv(sprintf("csv_FY/join_pm_median_%s_region%s.csv", year, region))
   } else if (method == "hybrid") {
     df_median = readr::read_csv("csv_FY/hybrid_gsa_national_cbecs_median.csv") %>%
+      dplyr::mutate(`Cat`=as.character(`Cat`)) %>%
       na.omit() %>%
       dplyr::select(-`source`) %>%
       {.}
     df <- df %>%
+      dplyr::mutate(`Cat`=as.character(`Cat`)) %>%
       dplyr::left_join(df_median, by=c("Building_Type", "Cat")) %>%
       {.}
   }
   df <- df %>%
     dplyr::mutate(`Potential_Saving` = (`eui_total` - `eui_median`) * `Gross_Sq.Ft` * (`Total_(Cost)` / `Total_(kBtu)`)) %>%
-    dplyr::mutate(`Potential_Saving` = `Potential_Saving` * 1e-6) %>%
+    ## dplyr::mutate(`Potential_Saving` = `Potential_Saving` * 1e-6) %>%
     dplyr::mutate(`Building_Number` = ifelse(`Cat` == "I", sprintf("(I) %s", `Building_Number`), `Building_Number`)) %>%
+    ## round to inaccurating the result XD
+    dplyr::mutate(`Potential_Saving`=`Potential_Saving` %/% mod * mod) %>%
     dplyr::arrange(desc(`Potential_Saving`)) %>%
     {.}
   df %>%
-    dplyr::select(`Building_Number`, `Building_Type`, `eui_total`, `eui_median`, `Potential_Saving`) %>%
+    dplyr::select(`Building_Number`, `Building_Type`, `eui_total`, `eui_median`, `Potential_Saving`, `Gross_Sq.Ft`, `Total_(Cost)`) %>%
+    dplyr::mutate(`dollar / sqft` = round(`Total_(Cost)` / `Gross_Sq.Ft`, 2)) %>%
+    dplyr::mutate_at(vars(`Potential_Saving`, `Gross_Sq.Ft`, `Total_(Cost)`, `eui_total`), function (x) format(x, big.mark=",", trim=TRUE)) %>%
     readr::write_csv(sprintf("csv_FY/dollar_saving_%s_median_%s_region%s.csv", method, year, region))
-  df %>%
-    dplyr::select(`Building_Number`, `Building_Type`, `eui_total`, `eui_median`, `Potential_Saving`) %>%
-    head() %>%
-    print()
+  ## df %>%
+  ##   dplyr::select(`Building_Number`, `Building_Type`, `eui_total`, `eui_median`, `Potential_Saving`) %>%
+  ##   head() %>%
+  ##   print()
   ## get the default palette
   pal_values <- scales::hue_pal()(2)
   dfTop <- df %>%
@@ -609,29 +615,93 @@ dollar_saving <- function(category, type, year, region, method="own", topn=10, b
     dplyr::filter(`Potential_Saving` <= 0) %>%
     top_n(n=(-1)*botn) %>%
     {.}
-  df <- rbind(dfTop, dfBottom)
-  p <- df %>%
+  options("scipen"=100, "digits"=4)
+  ## df <- rbind(dfTop, dfBottom)
+  max_abs = max(max(dfTop$`Potential_Saving`), max((-1) * dfBottom$`Potential_Saving`))
+  print(max_abs)
+  p <- dfTop %>%
     ## na.omit() %>%
-    dplyr::mutate(`Status` = ifelse(`Potential_Saving` <= 0, "Doing Fine", "Needs Attention")) %>%
-    ggplot2::ggplot(aes(x = reorder(`Building_Number`, `Potential_Saving`), y=`Potential_Saving`,
-                        fill=`Status`)) +
-    ggplot2::geom_bar(stat="identity") +
-    ggplot2::coord_flip() +
-    ggplot2::ylab("Million Dollar") +
+    ggplot2::ggplot(ggplot2::aes(x = reorder(`Building_Number`, `Potential_Saving`), y=`Potential_Saving`,
+                                 label=sprintf("$%s/year", format(`Potential_Saving`, big.mark=",", trim=TRUE)),
+                                 )) +
+    ggplot2::geom_bar(stat="identity", fill="#F89728") +
+    ggplot2::geom_text(family=fontFamily, size=fontsize/3.3, hjust=-0.1, colour="#303030") +
+    ggplot2::theme()
+  yranges = (ggplot2::ggplot_build(p)$layout$panel_ranges[[1]]$y.range)
+  ## ylimit = yranges + c(yleftLimit, yrightLimit)
+  if (plotGreen) {
+    ylimit = c(0, (max_abs + yadjust))
+  } else {
+    ylimit = yranges + c(0, yadjust)
+  }
+  p <- p +
+    ggplot2::coord_flip(ylim=ylimit) +
     ggplot2::xlab("Building Number") +
-    ggplot2::ggtitle(sprintf("Potential dollar saving%s (%s)", regionTag, method)) +
-    ## reverse the defulat color palette
-    ggplot2::scale_fill_manual(values = rev(pal_values)) +
+    ## ggplot2::ggtitle(sprintf("Potential dollar saving%s (%s)", regionTag, method)) +
+    ggplot2::ggtitle("Hypothetical savings: estimated annual savings \nif your building's EUI were reduced to national median") +
     ggplot2::theme_bw() +
-    ggplot2::theme(legend.position=legendloc)
-    ## head() %>%
+    ggplot2::theme(legend.position=legendloc, panel.grid.major = ggplot2::element_blank(),
+                    panel.grid.minor = ggplot2::element_blank(), panel.border = ggplot2::element_blank(),
+                    axis.title = ggplot2::element_blank(),
+                    text=ggplot2::element_text(size=fontsize, family=fontFamily)
+                    )
   print(p)
   if (missing(region)) {
-    ggsave(sprintf(file="region_report_img/regional/%s_median_potential_dollar_", method))
+    ggsave(sprintf(file="region_report_img/regional/%s_median_potential_dollar_orange", method))
   } else {
-    ggsave(file=sprintf("region_report_img/regional/%s_median_potential_dollar_region_%s.png", method, region),
-           width=5, height=8, units = "in")
+    ggplot2::ggsave(file=sprintf("region_report_img/regional/%s_median_potential_dollar_region_%s_orange.png", method, region), width=5, height=3, units = "in")
   }
+  if (plotGreen) {
+    p <- dfBottom %>%
+      ## na.omit() %>%
+      dplyr::mutate(`Potential_Saving` = `Potential_Saving`) %>%
+      ggplot2::ggplot(ggplot2::aes(x = reorder(`Building_Number`, `Potential_Saving`), y=`Potential_Saving`,
+                                  label=sprintf("$%s/year", format(`Potential_Saving`, big.mark=",", trim=TRUE)),
+                                  )) +
+      ggplot2::geom_bar(stat="identity", fill="#8CC482") +
+      ggplot2::geom_text(family=fontFamily, size=fontsize/3, hjust=1.1) +
+      ggplot2::theme()
+    yranges = (ggplot2::ggplot_build(p)$layout$panel_ranges[[1]]$y.range)
+    ## ylimit = yranges + c(yleftLimit, yrightLimit)
+    ylimit = c((-1)*(max_abs + yadjust), 0)
+    p <- p +
+      ggplot2::coord_flip(ylim=ylimit) +
+      ggplot2::scale_x_discrete(position = "top") +
+      ggplot2::xlab("Building Number") +
+      ## ggplot2::ggtitle(sprintf("Potential dollar saving%s (%s)", regionTag, method)) +
+      ggplot2::theme_bw() +
+      ggplot2::theme(legend.position=legendloc, panel.grid.major = ggplot2::element_blank(),
+                    panel.grid.minor = ggplot2::element_blank(), panel.border = ggplot2::element_blank(),
+                    axis.title = ggplot2::element_blank(),
+                    text=ggplot2::element_text(size=fontsize, family=fontFamily)
+                    )
+    print(p)
+    if (missing(region)) {
+      ggsave(sprintf(file="region_report_img/regional/%s_median_potential_dollar_green", method))
+    } else {
+      ggplot2::ggsave(file=sprintf("region_report_img/regional/%s_median_potential_dollar_region_%s_green.png", method, region), width=5, height=3, units = "in")
+    }
+  }
+  ## ggplot2::geom_text(hjust=hjust)
+  ## yranges = (ggplot2::ggplot_build(p)$layout$panel_ranges[[1]]$y.range)
+  ## print("default y range of plotting")
+  ## print(yranges)
+  ## ylimit = yranges + c(yleftLimit, yrightLimit)
+  ## p <- p +
+  ##   ggplot2::coord_flip(ylim=ylimit) +
+  ##   ggplot2::ylab("Dollar") +
+  ##   ## ggplot2::ylab("Million Dollar") +
+  ##   ggplot2::xlab("Building Number") +
+  ##   ## ggplot2::ggtitle(sprintf("Potential dollar saving%s (%s)", regionTag, method)) +
+  ##   ## reverse the defulat color palette
+  ##   ggplot2::scale_fill_manual(values = c("#8CC482", "#F89728")) +
+  ##   ggplot2::theme_bw() +
+  ##   ggplot2::theme(legend.position=legendloc, panel.grid.major = ggplot2::element_blank(),
+  ##                  panel.grid.minor = ggplot2::element_blank(), panel.border = ggplot2::element_blank(),
+  ##                  axis.title = ggplot2::element_blank(),
+  ##                  text=ggplot2::element_text(size=fontsize, family=fontFamily)
+  ##                  )
+    ## head() %>%
 }
 
 #' Summary table own median vs cbecs median
