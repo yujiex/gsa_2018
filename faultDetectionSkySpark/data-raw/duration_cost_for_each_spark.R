a4c6736c93a8454d3d9e7e9811df6153f7fe506a
yujiex <shenqing1225@gmail.com>
2019-05-24 11:50:12 -0400

    Initial commit

---
+++ faultDetectionSkySpark/data-raw/duration_cost_for_each_spark.R
@@ -0,0 +1,38 @@
+library("dplyr")
+library("readr")
+
+reg.result = readr::read_csv("reg_result/allrules.csv") %>%
+  tibble::as.tibble() %>%
+  {.}
+
+reg.result <- reg.result %>%
+  dplyr::rename(rule=covariate) %>%
+  tidyr::spread(occtype, coefficient) %>%
+  dplyr::filter(!(rule %in% c("(Intercept)", "F"))) %>%
+  {.}
+
+buildings = reg.result %>%
+  dplyr::distinct(building) %>%
+  .$building
+
+length(buildings)
+
+all.rule <- readr::read_csv("all_building_rule_2018.csv") %>%
+  tibble::as.tibble() %>%
+  dplyr::filter(building %in% buildings) %>%
+  {.}
+
+all.rule %>%
+  dplyr::filter(building %in% buildings) %>%
+  dplyr::select(rule, eCost, dur) %>%
+  dplyr::filter(!is.na(eCost)) %>%
+  dplyr::group_by(rule) %>%
+  dplyr::summarise_all(sum) %>%
+  dplyr::ungroup() %>%
+  dplyr::rename(duration.hour=dur) %>%
+  head()
+
+rule.summary <- readr::read_csv("rule_summary.csv") %>%
+  tibble::as.tibble() %>%
+  {.}
+
